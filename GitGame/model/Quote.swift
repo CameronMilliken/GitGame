1e5799f7d2d5f23df9e3a16bf00c907957565a78
CameronMilliken <cameron.milliken@gmail.com>
2019-01-10 14:50:47 -0700

    Creates model and model controller.

---
+++ GitGame/model/Quote.swift
@@ -0,0 +1,18 @@
+//
+//  Quote.swift
+//  GitGame
+//
+//  Created by Cameron Milliken on 1/10/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import Foundation
+
+class Quote {
+    let quote: String
+    init(quote:String) {
+        self.quote = quote
+    }
+    
+
+}
