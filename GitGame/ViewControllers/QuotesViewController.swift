d3b908bff0dbb0ca1022e03147b081e9f6a5a01d
Brady <bradjbentley@gmail.com>
2019-01-10 14:20:19 -0700

    adds the StoryBoards and the ViewControllers linked with each view.

---
+++ GitGame/ViewControllers/QuotesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  QuotesViewController.swift
+//  GitGame
+//
+//  Created by Brady Bentley on 1/10/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    // MARK: - ViewLifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    @IBAction func ronSwansonButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func orNotButtonTapped(_ sender: Any) {
+    }
+    
+}
